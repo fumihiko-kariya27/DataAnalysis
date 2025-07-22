872ffc0da9521d89c3de954da5037398713edeaa
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2025-07-22 22:49:01 +0900

    JS処理追加

---
+++ ecSiteAnalysis/static/js/mouseEvent.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tableRows = document.querySelectorAll("table tr");
+    tableRows.forEach((row, index) => {
+        if (index === 0){
+            // ヘッダー行の設定はスキップする
+            return;
+        }
+
+        row.addEventListener("mouseenter", () => {
+            row.style.backgroundcolor = "#f0f8ff";
+        });
+
+        row.addEventListener("mouseleave", () => {
+            row.style.backgroundcolor = "";
+        });
+
+        row.addEventListener("click", () => {
+            alert("購入者：" + row.cells[0].innerText + "\n商品名：" + row.cells[2].innerText + "\n数量　：" + row.cells[4].innerText);
+        });
+    })
+});
